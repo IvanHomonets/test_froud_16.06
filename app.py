157847d799defd5fa64f5e3bdb2cd8e579e67f4f
Ivan Homonets <32175155+IvanHomonets@users.noreply.github.com>
2020-06-16 17:48:32 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ import pandas as pd
 from flask import Flask, jsonify, request
 import pickle
 
-
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, learning_curve
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.metrics import roc_auc_score, roc_curve, auc as sklearn_auc, classification_report, precision_score, recall_score, f1_score, confusion_matrix, accuracy_score
+import json
+from flask import Response
 
 # load model
 model = pickle.load(open('model.pkl','rb'))
